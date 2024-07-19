b6dac8c277dface23807607777cf0207fcdeb409
HAN ZAW HTET <hanzawhtet317@gmail.com>
2024-07-19 16:07:59 +0630

    App.jsx updated and api connected

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,82 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect } from "react";
 
-import { Box, List, IconButton, InputAdornment, OutlinedInput, Alert, Badge } from '@mui/material';
 import {
-	List as ListIcon,
-	AddCircle as AddIcon,
-} from "@mui/icons-material";
+	Box,
+	List,
+	IconButton,
+	InputAdornment,
+	OutlinedInput,
+	Alert,
+	Badge,
+	Typography,
+} from "@mui/material";
+import { List as ListIcon, AddCircle as AddIcon } from "@mui/icons-material";
 import AssignmentIcon from "@mui/icons-material/Assignment";
-import Item from './components/item';
+import Item from "./components/item";
 
-export default function App(){
-  const [data, setData] = useState([
-    {_id: 1, name: "Read books", done: false},
-    {_id: 2, name: "Code X-app", done: false},
-    {_id: 3, name: "Watch tutorials", done: false},
-    {_id: 4, name: "Reset", done: true},
-  ]);
+const api = "http://localhost:8181/tasks";
 
-  const inputRef = useRef();
+export default function App() {
+	//   const [data, setData] = useState([
+	//     {_id: 1, name: "Read books", done: false},
+	//     {_id: 2, name: "Code X-app", done: false},
+	//     {_id: 3, name: "Watch tutorials", done: false},
+	//     {_id: 4, name: "Reset", done: true},
+	//   ]);
+	const [ data, setData ] = useState([]);
+	const [ loading, setLoading ] = useState(true);
+	const [ hasError, setHasError ] = useState(false)
+	const inputRef = useRef();
 
-  const add = async name => {
-    const id = data.length + 1;
-    setData([...data, {_id: id, name, done: false}]);
-  };
+	useEffect(() => {
+		(async () => {
+			try {
+				const res = await fetch(api);
+				if (res.ok) {
+					const data = await res.json();
+					setData(data);
+					setLoading(false);
+					setHasError(false);
+				} else {
+					setLoading(false);
+					setHasError(true);
+				}
+			} catch (error) {
+				setHasError(true);
+			}
+		})();
+	}, []);
 
-  const remove = _id => {
-	setData(data.filter(item => item._id !== _id));
-  };
+	const add = async (name) => {
+		const id = data.length + 1;
+		setData([...data, { _id: id, name, done: false }]);
+	};
 
-  const toggle = _id => {
-	setData(data.map(item => {
-		if(item._id === _id) item.done = !item.done;
-		return item;
-	}));
-  };
+	const remove = (_id) => {
+		setData(data.filter((item) => item._id !== _id));
+	};
 
-  return (
+	const toggle = (_id) => {
+		setData(
+			data.map((item) => {
+				if (item._id === _id) item.done = !item.done;
+				return item;
+			}),
+		);
+	};
+
+	return (
 		<Box>
-			<Box sx={{ padding: 5 }}>
+			<Box sx={{ margin: 0, padding: 2 }}>
 				<h1>
 					Todo
 					<Badge
 						color="error"
 						badgeContent={
 							data.filter((item) => !item.done)
-							.length
+								.length
 						}
-						sx={{m: 1}}
-						>
+						sx={{ m: 1 }}>
 						<AssignmentIcon />
 					</Badge>
 				</h1>
@@ -69,6 +99,18 @@ export default function App(){
 						}
 					/>
 				</form>
+				{loading && (
+					<Alert sx={{ justifyContent: "center" }}>
+						<Typography>Loading......</Typography>
+					</Alert>
+				)}
+				{hasError && (
+					<Alert
+						severity="error"
+						sx={{ mb: 2 }}>
+						Something went wrong
+					</Alert>
+				)}
 				<List>
 					{data
 						.filter((item) => !item.done)
@@ -100,5 +142,5 @@ export default function App(){
 				</List>
 			</Box>
 		</Box>
-  );
+	);
 }
